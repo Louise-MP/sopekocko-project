95cec7ee61ce42346483386fd4cd2129758bc243
Louise Mpili <louise.mpili@uniqueheritage.fr>
2021-03-07 22:52:18 +0100

    ajout router sauce

---
+++ backend/routes/auth.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const userCtrl = require('../controllers/user');
+
+router.post('/signup', userCtrl.signup);// envoie les infos (email + password) d'un nouvel utilisateur
+router.post('/login', userCtrl.login);// envoie les infos d'un utilisateur existant
+
+module.exports = router;
